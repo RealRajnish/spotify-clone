098d6116404b97c8a3374270df415faa68f8c00e
RealRajnish <rajnishf3@gmail.com>
2023-09-10 14:19:32 +0530

    -upload modal and functionality implemented

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,9 +1,22 @@
 "use client";
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user, subscription } = useUser();
+
   const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    // TODO: Check for subsciption
+    return uploadModal.onOpen();
     //handle upload later
   };
   return (
